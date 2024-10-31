faacf8d78ebe21bb045c6f48fff738ca79ddc487
GrigoryanDav <grigoryand219@gmail.com>
2024-10-31 18:37:15 +0400

    Add Register for my app

---
+++ src/components/Register/index.js
@@ -0,0 +1,50 @@
+import { Input, Form, Button } from "antd";
+import AuthWrapper from "../AuthWrapper";
+import registerBanner from '../../core/images/auth-register.jpg'
+
+const Register = () => {
+    return (
+        <AuthWrapper title='Sign Up' banner={registerBanner}>
+            <Form>
+                <Form.Item
+                label='User Name'
+                name='userName'
+                rules={[{
+                    required: true,
+                    message: 'Please input your User Name'
+                }]}
+                >
+                    <Input type="text" placeholder="User Name" />
+                </Form.Item>
+
+                <Form.Item
+                label='Email'
+                name='email'
+                rules={[{
+                    required: true,
+                    message: 'Please input your Email'
+                }]}
+                >
+                    <Input type="email" placeholder="Email" />
+                </Form.Item>
+
+                <Form.Item
+                label='Password'
+                name='password'
+                rules={[{
+                    required: true,
+                    message: 'Please input your Password'
+                }]}
+                >
+                    <Input.Password placeholder="Password" />
+                </Form.Item>
+
+                <Button>
+                    Sign Up
+                </Button>
+            </Form>
+        </AuthWrapper>
+    )
+}
+
+export default Register
