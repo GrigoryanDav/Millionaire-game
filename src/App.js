59823a0be18fa1038f19f8eaaeb1d5f387a98bab
GrigoryanDav <grigoryand219@gmail.com>
2024-10-31 21:16:34 +0400

    Add LoadingWrapper component and integrate in App

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { AuthContext } from "./context/authContext";
 import { useEffect, useState } from "react";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
+import LoadingWrapper from "./components/LoadingWrapper";
 import MainLayout from "./components/MainLayout";
 import Register from "./components/Register";
 import './styles/global.css'
@@ -13,32 +14,32 @@ import './styles/global.css'
 
 const App = () => {
   const [isAuth, setIsAuth] = useState(false)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsAuth(true)
-      } else {
-        setIsAuth(false)
-      }
+      setLoading(false)
+      setIsAuth(Boolean(user))
     })
   }, [])
 
   return (
     <AuthContext.Provider value={{ isAuth }}>
-      <RouterProvider
-        router={
-          createBrowserRouter(
-            createRoutesFromElements(
-              <Route path="/" element={<MainLayout />} >
-                <Route path={ROUTE_CONSTANTS.LOGIN} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Login />} />
-                <Route path={ROUTE_CONSTANTS.REGISTER} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Register />} />
-                <Route path={ROUTE_CONSTANTS.HOMEPAGE} element={isAuth ? <HomePage /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
-              </Route>
+      <LoadingWrapper loading={loading}>
+        <RouterProvider
+          router={
+            createBrowserRouter(
+              createRoutesFromElements(
+                <Route path="/" element={<MainLayout />} >
+                  <Route path={ROUTE_CONSTANTS.LOGIN} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Login />} />
+                  <Route path={ROUTE_CONSTANTS.REGISTER} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Register />} />
+                  <Route path={ROUTE_CONSTANTS.HOMEPAGE} element={isAuth ? <HomePage /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
+                </Route>
+              )
             )
-          )
-        }
-      />
+          }
+        />
+      </LoadingWrapper>
     </AuthContext.Provider>
   )
 }
