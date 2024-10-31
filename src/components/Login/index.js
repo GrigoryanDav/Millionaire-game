64a979413ae70d9b20124766f9175c900d532ad3
GrigoryanDav <grigoryand219@gmail.com>
2024-10-31 18:30:47 +0400

    Add Login component and images for auth

---
+++ src/components/Login/index.js
@@ -0,0 +1,41 @@
+import AuthWrapper from "../AuthWrapper"
+import { Form, Input, Button } from 'antd'
+import loginBanner from '../../core/images/auth-login.jpg'
+
+
+
+const Login = () => {
+    return (
+        <AuthWrapper title='Sign In' banner={loginBanner}>
+            <Form>
+                <Form.Item
+                label='Email'
+                name='email'
+                rules={[{
+                    required: true,
+                    message: 'Please input your Email'
+                }]}
+                >
+                    <Input type="email" placeholder="Email" />
+                </Form.Item>
+
+                <Form.Item
+                label='Password'
+                name='password'
+                rules={[{
+                    required: true,
+                    message: 'Please input your Password'
+                }]}
+                >
+                    <Input.Password placeholder="Password" />
+                </Form.Item>
+
+                <Button type="primary" htmlType="submit">
+                    Sign In
+                </Button>
+            </Form>
+        </AuthWrapper>
+    )
+}
+
+export default Login
