ecedb646baff8d370ebc88b233461acbe7676442
GrigoryanDav <grigoryand219@gmail.com>
2024-11-03 21:22:10 +0400

    Get user info from firestore and change the routing between pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Navigate } from "react-router-dom";
-import { ROUTE_CONSTANTS } from "./core/utils/constants";
+import { ROUTE_CONSTANTS, FIRESTORE_PATH_NAMES } from "./core/utils/constants";
 import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./services/firebase";
+import { auth, db } from "./services/firebase";
+import { doc, getDoc } from 'firebase/firestore'
 import { AuthContext } from "./context/authContext";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import LoadingWrapper from "./components/LoadingWrapper";
+import MainGame from "./components/MainGame";
 import MainLayout from "./components/MainLayout";
 import Register from "./components/Register";
 import './styles/global.css'
@@ -15,16 +17,28 @@ import './styles/global.css'
 const App = () => {
   const [isAuth, setIsAuth] = useState(false)
   const [loading, setLoading] = useState(true)
+  const [userInfo, setUserInfo] = useState([])
+
+  const handleGetUserData = useCallback(async (uid) => {
+    const docRef = doc(db, FIRESTORE_PATH_NAMES.REGISTERED_USERS, uid)
+    const response = await getDoc(docRef)
+
+    if (response.exists()) {
+      setUserInfo(response.data())
+    }
+  }, [])
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
+      user?.uid && handleGetUserData(user.uid)
+
       setLoading(false)
       setIsAuth(Boolean(user))
     })
-  }, [])
+  }, [handleGetUserData])
 
   return (
-    <AuthContext.Provider value={{ isAuth }}>
+    <AuthContext.Provider value={{ isAuth, userInfo, handleGetUserData }}>
       <LoadingWrapper loading={loading}>
         <RouterProvider
           router={
@@ -33,7 +47,8 @@ const App = () => {
                 <Route path="/" element={<MainLayout />} >
                   <Route path={ROUTE_CONSTANTS.LOGIN} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Login />} />
                   <Route path={ROUTE_CONSTANTS.REGISTER} element={isAuth ? <Navigate to={ROUTE_CONSTANTS.HOMEPAGE} /> : <Register />} />
-                  <Route path={ROUTE_CONSTANTS.HOMEPAGE} element={isAuth ? <HomePage /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
+                  <Route path={ROUTE_CONSTANTS.HOMEPAGE} element={isAuth ? <HomePage userInfo={userInfo} /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
+                  <Route path={ROUTE_CONSTANTS.PLAY} element={isAuth ? <MainGame /> : <Navigate to={ROUTE_CONSTANTS.LOGIN} />} />
                 </Route>
               )
             )
