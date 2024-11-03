5ca7aa2c75549928eb1764167700feade21e3e5d
GrigoryanDav <grigoryand219@gmail.com>
2024-11-03 21:14:19 +0400

    Add HomePage content and logic

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,7 +1,40 @@
-const HomePage = () => {
+import { Button } from 'antd'
+import { signOut } from 'firebase/auth'
+import { auth } from '../../services/firebase'
+import { useNavigate, Outlet } from 'react-router-dom'
+import { ROUTE_CONSTANTS } from '../../core/utils/constants'
+import gameLogo from '../../core/images/millionaire-game-logo.jpg'
+import './index.css'
+
+
+const HomePage = ({ userInfo }) => {
+    const navigate = useNavigate()
+
+    const handleSignOut = async () => {
+        try {
+            await signOut(auth)
+        } catch (error) {
+            console.log(error, 'SignOut error!')
+        }
+    }
+
+    const handleStart = () => {
+        navigate(ROUTE_CONSTANTS.PLAY)
+        console.log('navigate')
+    }
+
     return (
-        <div>
-            <h2>Home Page will be ready soon...</h2>
+        <div className="home_container">
+            <div className='home_img_container' style={{ backgroundImage: `url(${gameLogo})` }}></div>
+            <h2>
+                Hello {userInfo.userName}․ <br />
+                Test your knowledge by playing who wants to be a millionaire game.
+            </h2>
+            <div className='home_button_container'>
+                <Button type='primary' onClick={handleStart}>Start</Button>
+                <Button type='primary' onClick={handleSignOut}>Log Out</Button>
+            </div>
+            <Outlet />
         </div>
     )
 }
