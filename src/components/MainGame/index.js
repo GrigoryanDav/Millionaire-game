1154a26d77c4328909b829e7178bdd530e2a6a54
GrigoryanDav <grigoryand219@gmail.com>
2024-11-03 22:19:42 +0400

    Set up initial structure for MainGame component and add styles

--- src/components/MainGame/index.js
+++ src/components/MainGame/index.js
@@ -1,7 +1,25 @@
+import { questionsArray } from "../../core/utils/constants"
+import './index.css'
+
 const MainGame = () => {
     return (
-        <div>
-            <h2>This page will be ready soon...</h2>
+        <div className="game_container">
+            <div className="question_title">{questionsArray[0].question}</div>
+            <div className="options_container">
+                <div className="options">
+                    <button className="option"><span>A:</span> {questionsArray[0].options[0]}</button>
+                    <button className="option"><span>C:</span> {questionsArray[0].options[2]}</button>
+                </div>
+                <div className="options">
+                    <button className="option"><span>B:</span> {questionsArray[0].options[1]}</button>
+                    <button className="option"><span>D:</span> {questionsArray[0].options[3]}</button>
+                </div>
+            </div>
+            <div className="help_container">
+                <button className="help">50/50</button>
+                <button className="help">Hall help</button>
+            </div>
+            <div className="round_container">1/10</div>
         </div>
     )
 }
