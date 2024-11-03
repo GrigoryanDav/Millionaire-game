fd339725bbe7793f8136f716abcb083630a7a12a
GrigoryanDav <grigoryand219@gmail.com>
2024-11-03 21:19:44 +0400

    Add constants for firebase and for MainGame component

--- src/core/utils/constants.js
+++ src/core/utils/constants.js
@@ -2,5 +2,9 @@ export const ROUTE_CONSTANTS = {
     LOGIN: '/login',
     REGISTER: '/register',
     HOMEPAGE: '/home',
+    PLAY: '/home/play',
 }
 
+export const FIRESTORE_PATH_NAMES = {
+    REGISTERED_USERS: 'registered_users'
+}
