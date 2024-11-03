4e1e77b25ec4b30552a5258cd1d38341303d03ea
GrigoryanDav <grigoryand219@gmail.com>
2024-11-03 21:09:58 +0400

    Add firestore configurations in firebase

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
@@ -13,8 +14,10 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
 const auth = getAuth(app)
 
 export {
-    auth
+    auth,
+    db,
 }
