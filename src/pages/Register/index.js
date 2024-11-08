670133d982c3e0737de30174f2d548f2cdc3f864
GrigoryanDav <grigoryand219@gmail.com>
2024-11-08 23:24:45 +0400

    Add progress bar for password strength

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,4 +1,4 @@
-import { Input, Form, Button, Flex } from "antd";
+import { Input, Form, Button, Flex, Progress } from "antd";
 import AuthWrapper from "../../components/AuthWrapper";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../../services/firebase";
@@ -12,10 +12,27 @@ const Register = () => {
     const [loading, setLoading] = useState(false)
     const [form] = Form.useForm()
     const navigate = useNavigate()
+    const [passwordStrength, setPasswordStrength] = useState(0)
+
+
+    const checkPasswordStrength = (value) => {
+        let strength = 0
+        if (value.length >= 6) strength += 20
+        if (value.length >= 10) strength += 20
+        if (/[A-Z]/.test(value)) strength += 20
+        if (/[0-9]/.test(value)) strength += 20
+        if (/[!@#$%^&*]/.test(value)) strength += 20
+
+        setPasswordStrength(strength)
+    }
+
+    const handlePasswordChange = (e) => {
+        checkPasswordStrength(e.target.value.trim())
+    }
 
     const handleRegister = async (values) => {
         setLoading(true)
-        const {userName, email, password } = values
+        const { userName, email, password } = values
         try {
             const response = await createUserWithEmailAndPassword(auth, email, password)
             const { uid } = response.user
@@ -23,7 +40,7 @@ const Register = () => {
             await setDoc(createdDoc, {
                 uid, userName, email
             });
-            
+
             navigate(ROUTE_CONSTANTS.LOGIN)
         } catch (e) {
             console.log(e)
@@ -31,6 +48,7 @@ const Register = () => {
             setLoading(false)
         }
     }
+
     return (
         <AuthWrapper title='Sign Up' banner={registerBanner}>
             <Form layout="vertical" form={form} onFinish={handleRegister}>
@@ -63,13 +81,36 @@ const Register = () => {
                         required: true,
                         message: 'Please input your Password'
                     },
-                {
-                    pattern: regexpValidation,
-                    message: 'Wrong Password'
-                }]}
+                    {
+                        pattern: regexpValidation,
+                        message: 'Password does not meet the criteria'
+                    }]}
                 >
-                    <Input.Password placeholder="Password" />
+                    <Input.Password placeholder="Password" onChange={handlePasswordChange} />
                 </Form.Item>
+
+                {
+                    form.getFieldValue('password') && (
+                        <Progress 
+                        percent={passwordStrength}
+                        showInfo={false}
+                        strokeColor={
+                            passwordStrength < 40 ? 'red' : passwordStrength < 80 ? 'orange' : 'green'
+                        }
+                        />
+                    )
+                }
+
+                {
+
+                    form.getFieldValue('password') && (
+                        <p style={{color : passwordStrength < 40 ? 'red' : passwordStrength < 80 ? 'orange' : 'green'}}>
+                            {
+                            passwordStrength < 40 ? 'Weak password' : passwordStrength < 80 ? 'Medium password' : 'Strong Password'
+                        }
+                        </p>
+                    )
+                }
 
                 <Flex justify="flex-end" align="center" gap='10px'>
                     <Link to={ROUTE_CONSTANTS.LOGIN}>Sign In</Link>
